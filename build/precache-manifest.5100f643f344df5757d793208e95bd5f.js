b333971a1fe669f06c022a94248978e04239f54f
Mikael <mikael.heininen@student.uwasa.fi>
2020-08-12 18:21:06 +0300

    removed :

---
+++ build/precache-manifest.5100f643f344df5757d793208e95bd5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6bdb20d20097927b5fc059c1d04420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5593a566d35fe58f110",
+    "url": "/static/js/2.ed42f439.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.ed42f439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49004b4854d8b49b935c",
+    "url": "/static/js/main.b6a68a63.chunk.js"
+  },
+  {
+    "revision": "29200bfcfb6dd25693c2",
+    "url": "/static/js/runtime-main.22c93f8a.js"
+  }
+]);
