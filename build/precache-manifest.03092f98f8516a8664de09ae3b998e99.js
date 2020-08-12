e01d350cf595675fb1d56871f83a20444335a7dc
Mikael <mikael.heininen@student.uwasa.fi>
2020-08-12 17:48:57 +0300

    updated readme

---
+++ build/precache-manifest.03092f98f8516a8664de09ae3b998e99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f897a970a575b826830e71f45c886103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5593a566d35fe58f110",
+    "url": "/static/js/2.ed42f439.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.ed42f439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7bb7249994ca5ab466",
+    "url": "/static/js/main.8053b126.chunk.js"
+  },
+  {
+    "revision": "29200bfcfb6dd25693c2",
+    "url": "/static/js/runtime-main.22c93f8a.js"
+  }
+]);
